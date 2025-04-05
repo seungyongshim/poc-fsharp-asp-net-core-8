23daaa21b4d2384baff14726dbe55ab33770e084
seungyongshim <seungyongshim@gmail.com>
2025-04-06 00:53:11 +0900

    HTTP 요청 처리 및 JSON 직렬화 개선
    `Microsoft.AspNetCore.Http` 네임스페이스를 추가하고,
    `JsonFSharpConverter`를 사용하여 새로운 타입들을 정의했습니다.
    `post_json` 함수를 추가하여 JSON 데이터를 POST하는 기능을 구현하였으며,
    `main` 함수에서 HTTP 클라이언트를 설정하는 방식과 루트 경로 요청 처리 방식을 변경했습니다.
    `post "/api/v2/users"` 핸들러에서 DTO를 사용하여 JSON 데이터를 전송하는 방식이 개선되었습니다.
    전체적으로 HTTP 요청 처리와 JSON 직렬화 관련 코드가 정리되었습니다.

--- src/webapi/Program.fs
+++ src/webapi/Program.fs
@@ -5,42 +5,50 @@ open System.Net.Http
 open System
 open System.Net.Http.Json
 open System.Text.Json.Serialization
-open Microsoft.AspNetCore.Http.Json
-open Microsoft.Extensions.Options
 
+[<JsonFSharpConverter>]
 type Name = Name of string
+
+[<JsonFSharpConverter>]
 type InternationalPhoneNumber = InternationalPhoneNumber of string
-type PhoneNumber = InternationalPhoneNumber of InternationalPhoneNumber
-
-
 
 type Dto = {
     Name: Name
-    PhoneNumber : PhoneNumber
+    PhoneNumber : InternationalPhoneNumber
 }
+
+let post_json (url:string) dto (http: HttpClient) = http.PostAsJsonAsync(url, dto)
 
 [<EntryPoint>]
 let main _ =
     let app = webApp {
         services (fun services _ ->
-            services.ConfigureHttpJsonOptions(fun options ->
-                options.SerializerOptions.Converters.Add(JsonFSharpConverter())
-            ) |> ignore
             services.AddHttpClient("reqres", fun (http: HttpClient) ->
-                http.BaseAddress <- Uri("https://reqres.in")
+                http.BaseAddress <- Uri "https://reqres.in"
             ).AddAsKeyed()
         )
         
-        get "/" (fun () -> task {
-            return Results.Ok("Hello World")
+        get "/" (fun (httpContext: HttpContext) -> task {
+            use http = httpContext.RequestServices.GetRequiredKeyedService<HttpClient> "reqres"
+
+            let request = post_json "/api/users" {| Name = "John Doe" |}
+            let! response = request http
+            let! json_res = response.Content.ReadFromJsonAsync<Dto>()
+
+            return Results.Ok {|
+                Name = json_res.Name
+            |}
         })
 
-        post "/api/v2/users" (fun (httpContext: HttpContext) ->
-                              fun (dto: Dto) -> backgroundTask {
-            use httpClient = httpContext.RequestServices.GetRequiredKeyedService<HttpClient>("reqres")
-            let options = httpContext.RequestServices.GetRequiredService<IOptions<JsonOptions>>().Value.SerializerOptions
+        post "/api/v2/users" (fun (httpContext: HttpContext) (dto: Dto) -> backgroundTask {
+            use http = httpContext.RequestServices.GetRequiredKeyedService<HttpClient> "reqres"
 
-            let! response = httpClient.PostAsJsonAsync("/api/users", dto, options)
+            let request = post_json "/api/users" {|
+                Name = dto.Name
+                PhoneNumber = dto.PhoneNumber
+            |}
+
+            let! response = http |> request
             let! stream = response.Content.ReadAsStreamAsync()
             return Results.Stream(stream, contentType= $"{response.Content.Headers.ContentType}")
         })
@@ -48,5 +56,3 @@ let main _ =
 
     app.Run()
     0 // Exit code
-
-
